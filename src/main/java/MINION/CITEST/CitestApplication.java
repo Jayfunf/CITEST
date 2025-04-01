427bee44c4f8baffd499aa8444cf1b9ab178acc5
Jayfunf <simh3077@gmail.com>
2025-04-01 19:47:35 +0900

    Test app

--- src/main/java/MINION/CITEST/CitestApplication.java
+++ src/main/java/MINION/CITEST/CitestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CitestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Minhyun");
 		SpringApplication.run(CitestApplication.class, args);
 	}
 
